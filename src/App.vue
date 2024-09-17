13e39b2f204364c0dbf16db8eaad20609322f419
Jamickin <93577753+Jamickin@users.noreply.github.com>
2024-09-17 12:09:35 +0200

    details

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup></script>
+
+<template>
+	<header>
+		<!-- <h1
+			class="text-2xl text-gray-400 text-center border-b-2 border-gray-300 py-2"
+		>
+			The Garden Bar
+		</h1> -->
+	</header>
+
+	<main>
+		<div class="flex flex-col gap-12">
+			<img
+				src="/images/2.png"
+				alt="brochuresecondpage"
+			/>
+			<img
+				src="/images/1.png"
+				alt="brochurefirstpage"
+			/>
+		</div>
+	</main>
+</template>
+
+<style scoped></style>
